580d7e6ecbfab190aacb7b9a9228541126dd96b5
Tyler Starling <tyler.a.starling@gmail.com>
2021-07-16 18:54:17 -0400

    made changes to allow objects entered by users to be pushed to array and notes to be assigned ids so they are able to be deleted'

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,29 @@
-const noteData = require('../db/noteData');
+const fs = require('fs')
+const uniqid = require('uniqid')
 
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    let noteJSON = require('../db/db.json')
+    app.get('/api/notes', (req, res) => res.json(noteJSON));
 
-    app.post('api/notes', (req, res) => {
-        noteData.push(req.body);
-        res.json(true);
+    app.post('/api/notes', (req, res) => {
+        req.body.id = uniqid();
+        noteJSON.push(req.body);
+        let newNote = JSON.stringify(noteJSON);
+        fs.writeFile(__dirname + '/../db/db.json', newNote, (err) => {
+            if (err) throw err;
+        });
+        res.end();
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        let filter = noteJSON.filter(note => {
+            return note.id != req.params.id
+        });
+        let newNote = JSON.stringify(filter);
+        fs.writeFileSync(__dirname + '/../db/db.json', newNote, (err) => {
+            if (err) throw err;
+        });
+        res.end();
     })
 };
